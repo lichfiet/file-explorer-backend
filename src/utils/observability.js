ca7187675b009f4c0b4a767198bcb6cf1a34de8f
lichfiet <sortaminty@gmail.com>
2024-12-03 19:15:59 -0700

    stuff

--- src/utils/observability.js
+++ src/utils/observability.js
@@ -5,6 +5,7 @@ const opentelemetry = require('@opentelemetry/sdk-node');
 const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
@@ -16,14 +17,20 @@ const exporterOptions = {
   },
 };
 
-const logExporter = new OTLPLogExporter({
-  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT + '/v1/logs', // Update for your backend
-});
+let otelUrl = "localhost:4317";
+
+if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
+  otelUrl = process.env.OTEL_EXPORTER_OTLP_ENDPOINT + '/v1/traces'
+}
+
+const logExporter = {
+  url: otelUrl
+};
 
 const traceExporter = new OTLPTraceExporter(exporterOptions)
 const sdk = new opentelemetry.NodeSDK({
   traceExporter,
-  logRecordProcessors: [new opentelemetry.logs.SimpleLogRecordProcessor(logExporter)],
+  logRecordProcessors: [new opentelemetry.logs.SimpleLogRecordProcessor(new OTLPLogExporter(logExporter))],
   instrumentations: [
     getNodeAutoInstrumentations(),
     new PinoInstrumentation({
