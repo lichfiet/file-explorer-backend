0419b5fbbde703830b9481995ac8c750f3bf2d5c
Trevor Lichfield <sortaminty@gmail.com>
2024-12-06 10:15:44 -0700

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,26 +1,5 @@
 const { logger } = require('./observability.js')
 
-console.error = function(message) {
-  logger.error(message);
-};
-
-console.warn = function(message) {
-  logger.warn(message);
-};
-
-console.log = function(message) {
-  logger.info(message);
-};
-
-console.debug = function(message) {
-  logger.debug(message);
-};
-
-console.info = function(message) {
-  logger.info(message);
-};
-
-console.log("Initialized Logging");
 
 module.exports = logger;
   
