a04d184425f0030e9ffea8bf1325d1ae2217e77a
lichfiet <sortaminty@gmail.com>
2024-12-06 00:34:19 -0700

    sdk stuff

--- src/utils/observability.js
+++ src/utils/observability.js
@@ -10,23 +10,6 @@ const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 const pino = require('pino')
 
-if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
-  const observability = require("./utils/observability.js");
-  observability.sdk.start();
-
-  process.on('SIGTERM', () => {
-    observability.sdk
-      .shutdown()
-      .then(() => console.log('Tracing terminated'))
-      .catch((error) => console.log('Error terminating tracing', error))
-      .finally(() => process.exit(0))
-  })
-  console.info("Initializing logging");
-}
-
-
-
-
 // Trace Exporter Configuration
 const exporterOptions = {
   url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT + '/v1/traces',
@@ -62,30 +45,23 @@ const sdk = new opentelemetry.NodeSDK({
   }),
 })
 
+
+if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
+  sdk.start();
+ß
+  process.on('SIGTERM', () => {
+    sdk.shutdown()
+      .then(() => console.log('Tracing terminated'))
+      .catch((error) => console.log('Error terminating tracing', error))
+      .finally(() => process.exit(0))
+  })
+  console.info("Initializing logging");
+}
+
 const logger = pino({
 
 })
 
 logger.info("test")
-
-console.error = function(message) {
-  logger.error(message);
-};
-
-console.warn = function(message) {
-  logger.warn(message);
-};
-
-console.log = function(message) {
-  logger.info(message);
-};
-
-console.debug = function(message) {
-  logger.debug(message);
-};
-
-console.info = function(message) {
-  logger.info(message);
-};
 
 module.exports = { sdk, logger };
