85c8dd6fb02b1073ef8d0c7f79e42dee124a6610
Trevor Lichfield <sortaminty@gmail.com>
2024-12-06 10:08:14 -0700

    Update observability.js

--- src/utils/observability.js
+++ src/utils/observability.js
@@ -8,7 +8,6 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
 const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const pino = require('pino')
 
 // Trace Exporter Configuration
 const exporterOptions = {
@@ -17,11 +16,9 @@ const exporterOptions = {
     'OT-Tracer-Span-Export': 'true',
   },
 };
-const traceExporter = new OTLPTraceExporter(exporterOptions)
-
-
 
 let otelUrl = "localhost:4317";
+
 if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
   otelUrl = process.env.OTEL_EXPORTER_OTLP_ENDPOINT
 }
@@ -30,27 +27,42 @@ const logExporter = {
   url: otelUrl + '/v1/logs'
 };
 
+const traceExporter = new OTLPTraceExporter(exporterOptions)
 const sdk = new opentelemetry.NodeSDK({
   traceExporter,
   logRecordProcessors: [new opentelemetry.logs.SimpleLogRecordProcessor(new OTLPLogExporter(logExporter))],
   instrumentations: [
     getNodeAutoInstrumentations(),
-    new PinoInstrumentation({
-      logLevel: process.env.LOG_LEVEL || 'info',
-    }),
+    new PinoInstrumentation(),
   ],
   resource: new Resource({
     // highlight-next-line
-    [SemanticResourceAttributes.SERVICE_NAME]: '',
+    [SemanticResourceAttributes.SERVICE_NAME]: 'node_app',
   }),
 })
 
+const pino = require('pino');
 const logger = pino({
+  level: `${(process.env.LOG_LEVEL).toLowerCase()}` || 'info', // log level for development
   transport: {
-    target: 'pino-pretty', // Optional: Makes logs human-readable in development
-  },
-})
+    target: 'pino-pretty'
+  }
+  });
 
-logger.info("test")
+console.error = function(message) {
+  logger.error(message);
+};
+
+console.warn = function(message) {
+  logger.warn(message);
+};
+
+console.log = function(message) {
+  logger.info(message);
+};
+
+console.debug = function(message) {
+  logger.debug(message);
+};
 
 module.exports = { sdk, logger };
